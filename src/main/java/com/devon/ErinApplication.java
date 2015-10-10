d69834b078647ebf3653255253993935f5e8ed10
Jose Alavez <josecarlos215@gmail.com>
2015-10-10 09:41:42 -0600

    init

--- src/main/java/com/devon/ErinApplication.java
+++ src/main/java/com/devon/ErinApplication.java
@@ -2,11 +2,19 @@ package com.devon;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.context.web.SpringBootServletInitializer;
 
 @SpringBootApplication
-public class ErinApplication {
+public class ErinApplication extends SpringBootServletInitializer {
 
-	public static void main(String[] args) {
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(ErinApplication.class);
+	}
+
+	public static void main(String[] args) throws Exception {
 		SpringApplication.run(ErinApplication.class, args);
 	}
+
 }
