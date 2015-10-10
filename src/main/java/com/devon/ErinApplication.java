039c3aaea75f46b39a05dfb93afae0baf290d025
Jose Alavez <josecarlos215@gmail.com>
2015-10-10 02:34:30 -0600

    init

---
+++ src/main/java/com/devon/ErinApplication.java
@@ -0,0 +1,12 @@
+package com.devon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ErinApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ErinApplication.class, args);
+    }
+}
---
+++ src/test/java/com/devon/ErinApplicationTests.java
@@ -0,0 +1,18 @@
+package com.devon;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.test.context.web.WebAppConfiguration;
+import org.springframework.boot.test.SpringApplicationConfiguration;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringApplicationConfiguration(classes = ErinApplication.class)
+@WebAppConfiguration
+public class ErinApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
