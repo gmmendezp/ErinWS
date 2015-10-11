fec0ff258f59c37b78626a9b1b13f76922b25072
Jose Alavez <josecarlos215@gmail.com>
2015-10-11 00:32:10 -0600

    Adding delete.

--- src/main/java/com/devon/controller/UserController.java
+++ src/main/java/com/devon/controller/UserController.java
@@ -5,6 +5,8 @@ import com.devon.repository.UserRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+
 @RestController
 @RequestMapping("/user")
 public class UserController {
@@ -21,5 +23,20 @@ public class UserController {
 	@RequestMapping(method = RequestMethod.GET, value = "{id}")
 	public User getUser(@PathVariable String id) {
 		return userRepository.findOne(id);
+	}
+
+	@RequestMapping(method = RequestMethod.GET)
+	public Collection<User> getUsers() {
+		return userRepository.findAll();
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE, value = "{id}")
+	public void deleteUser(@PathVariable String id) {
+		userRepository.delete(id);
+	}
+
+	@RequestMapping(method = RequestMethod.DELETE)
+	public void deleteAllUsers() {
+		userRepository.deleteAll();
 	}
 }
